c071101e3b8a078f9efba75204fa25ea7a7cedaf
Aqtoni <109670071+Aqtoni@users.noreply.github.com>
2023-01-10 17:03:15 +0200

    Version v2

---
+++ PostController.js
@@ -0,0 +1,18 @@
+import Post from "./Post.js";
+
+class PostController {
+    async create(req, res){
+        try {
+            const {author, title, content, picture} = req.body
+            const post = await Post.create({author, title, content, picture})
+            res.json(post)  
+            console.log(req.body);  
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+}
+
+
+
+export default new PostController();
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import express from "express"
 import mongoose from "mongoose";
+import Post from "./Post.js";
+import router from "./router.js";
 const PORT = 5000;
 const DB_URL = `mongodb+srv://user:user@cluster0.phluquy.mongodb.net/?retryWrites=true&w=majority`
 
 const app = express();
 
 app.use(express.json())
-
-app.post('/',(req, res) => {
-    console.log(req.body);
-    
-  res.status(200).json("Сервер работает 123")  
-} ) 
-
+app.use('/api', router)
+ 
 async function startApp(){
     try {
-      await  mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true })
+      await  mongoose.connect(DB_URL, /*{useUnifiedTopology: true, useNewUrlParser: true }*/)
         app.listen(PORT, () => console.log("Server started on port " + PORT));
     } catch (e) {
         console.log(e);
